4eebd0124704fa834cc9673b21d6c579fc3213fa
fdtomn <niuyuxian@163.com>
2015-01-29 09:04:55 +0800

    测试

---
+++ src/test/java/com/lb/test/TestPoi.java
@@ -0,0 +1,14 @@
+package com.lb.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPoi {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
