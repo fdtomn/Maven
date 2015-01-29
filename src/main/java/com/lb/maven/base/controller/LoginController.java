3169b3e94fa268f8116ecd6c12f6b3e3630a00cf
fdtomn <niuyuxian@163.com>
2015-01-29 08:58:20 +0800

    项目初始化

--- src/main/java/com/lb/maven/base/controller/LoginController.java
+++ src/main/java/com/lb/maven/base/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.lb.maven.base.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,15 +13,36 @@ import com.lb.maven.base.domain.UserInfo;
 @RequestMapping("/account")
 public class LoginController {
 
+	static Logger logger = Logger.getLogger(LoginController.class);
+	
+	/**
+	 * 跳转到登陆页面
+	 * @return
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String toLogin(){
+		logger.info("跳转到登陆.......");
 		return "login";
 	}
 	
+	/**
+	 * 登录操作
+	 * @param request
+	 * @param userInfo
+	 * @return
+	 */
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(HttpServletRequest request,UserInfo userInfo){
 		request.setAttribute("userInfo", userInfo);
-		return "home";
+		return "index";
 	}
 	
+	/**
+	 * 从Session中获取用户信息
+	 * @param request
+	 * @return
+	 */
+	public static UserInfo getUserInfo(HttpServletRequest request){
+		return (UserInfo)request.getSession().getAttribute("userInfo");
+	}
 }
