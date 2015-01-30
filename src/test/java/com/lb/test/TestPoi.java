5829f996826884de402f28c809af49d54c272d5e
fdtomn <etoak@163.com>
2015-01-30 15:27:37 +0800

    a

--- src/test/java/com/lb/test/TestPoi.java
+++ src/test/java/com/lb/test/TestPoi.java
@@ -1,14 +1,12 @@
 package com.lb.test;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TestPoi {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
